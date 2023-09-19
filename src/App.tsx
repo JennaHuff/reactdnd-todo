b64b5e1ae21d7059f550434df9a880d8c86c6e76
anton <antonjollant@aol.com>
2023-09-19 14:37:52 +0200

    added react-dnd-multi-backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { DndProvider } from "react-dnd";
+import { DndProvider } from "react-dnd-multi-backend";
 import "./App.css";
 import { TodoPage } from "./pages/todo/TodoPage";
 // import { HTML5Backend } from "react-dnd-html5-backend";
 import { Backgammon } from "./pages/backgammon/BackgammonPage";
-import { TouchBackend } from "react-dnd-touch-backend";
+// import { TouchBackend } from "react-dnd-touch-backend";
+import { HTML5toTouch } from "rdndmb-html5-to-touch";
 
 function App() {
     return (
         <>
-            <DndProvider backend={TouchBackend}>
+            <DndProvider options={HTML5toTouch}>
                 <Backgammon />
                 <TodoPage />
             </DndProvider>
