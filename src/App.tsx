2fa90a3fdb10d4e13fdfb0c45e57e6e7361d3967
anton <antonjollant@aol.com>
2023-09-19 13:59:32 +0200

    test touch backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { DndProvider } from "react-dnd";
 import "./App.css";
 import { TodoPage } from "./pages/todo/TodoPage";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { HTML5Backend } from "react-dnd-html5-backend";
 import { Backgammon } from "./pages/backgammon/BackgammonPage";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 function App() {
     return (
         <>
-            <DndProvider backend={HTML5Backend}>
+            <DndProvider backend={TouchBackend}>
                 <Backgammon />
                 <TodoPage />
             </DndProvider>
