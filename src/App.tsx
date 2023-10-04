1973034c6e780211b23d354e90fb2d7aea78c7a8
anton <antonjollant@aol.com>
2023-10-04 13:52:05 +0200

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { DndProvider } from "react-dnd-multi-backend";
 import "./App.css";
 import { TodoPage } from "./pages/todo/TodoPage";
-// import { HTML5Backend } from "react-dnd-html5-backend";
 import { Backgammon } from "./pages/backgammon/BackgammonPage";
-// import { TouchBackend } from "react-dnd-touch-backend";
 import { HTML5toTouch } from "rdndmb-html5-to-touch";
 
 function App() {
