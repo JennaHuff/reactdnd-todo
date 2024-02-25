6b7b6b17a17f86cfa31b98ceb7b36d48e114f097
anton <antonjollant@aol.com>
2024-02-25 15:27:10 +0100

    deleted backgammon in order to split projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { DndProvider } from "react-dnd-multi-backend";
 import "./App.css";
 import { TodoPage } from "./pages/todo/TodoPage";
-import { Backgammon } from "./pages/backgammon/BackgammonPage";
 import { HTML5toTouch } from "rdndmb-html5-to-touch";
 
 function App() {
     return (
         <>
             <DndProvider options={HTML5toTouch}>
-                <Backgammon />
                 <TodoPage />
             </DndProvider>
         </>
